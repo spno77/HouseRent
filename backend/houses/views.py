94b38627e957598d3e52b1de9492716ff311a95c
spno77 <pilonoti@yahoo.com>
2020-06-19 16:15:58 +0300

    Added Search and Ordering filters

--- backend/houses/views.py
+++ backend/houses/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
-
+from rest_framework import generics,filters
 
 from .models import House
 from .serializers import HouseSerializer
@@ -10,7 +9,9 @@ from .serializers import HouseSerializer
 class HouseList(generics.ListCreateAPIView):
 	queryset = House.objects.all()
 	serializer_class = HouseSerializer
-
+	filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+	search_fields = ['title','description']
+	ordering_fields = ['cost']
 
 class HouseDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = House.objects.all()
