9327f0cbcd0b6a0773879a0a975fcec12e85a8ff
spno77 <pilonoti@yahoo.com>
2020-08-16 21:05:53 +0300

    Changed ForeingKey in reviews to OneToOneField

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -10,7 +10,7 @@ class Review(models.Model):
 	date_created = models.DateField(auto_now=True)
 	rating		 = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
 
-	reservation  = models.ForeignKey('reservations.Reservation',on_delete=models.CASCADE,related_name='reservation') 
+	reservation  = models.OneToOneField('reservations.Reservation',on_delete=models.CASCADE,related_name='reservation') 
 	reviewer     = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name='reviewer')
 
 	def __str__(self):
