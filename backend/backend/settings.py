21b920062b94ef06c0e98450af466045d8a1ebf5
spno77 <pilonoti@yahoo.com>
2020-08-16 18:56:16 +0300

    Begab the Django part of Review app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'houses',
     'users',
     'reservations',
+    'reviews',
 ]
 
 MIDDLEWARE = [
---
+++ backend/reviews/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from PIL import Image
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
+
+from reservations.models import Reservation
+
+class Review(models.Model):
+	content      = models.CharField(max_length=100)
+	date_created = models.DateField(auto_now=True)
+	rating		 = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
+
+	reservation  = models.OneToOneField('reservations.Reservation',on_delete=models.CASCADE,primary_key=True) 
+	reviewer     = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name='reviewer')
+
+	def __str__(self):
+		return self.content	
